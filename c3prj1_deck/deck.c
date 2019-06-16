1862960db89520cc90d2fc284505e56634b5846b
Coursera Learner <nobody@nowhere.nul>
2019-06-16 20:15:51 +0000

    attempt1: good accuracies.. little annoyed that the least common hand is 0.0001% worse than Drews

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -2,18 +2,39 @@
 #include <stdlib.h>
 #include <assert.h>
 #include "deck.h"
-void print_hand(deck_t * hand){
 
+void print_hand(deck_t * hand){
+  // print the content of a hand
+  for (int n = 0; n < hand->n_cards; n++) {
+    print_card(*hand->cards[n]);
+  }  // for: all cards in hand
 }
 
 int deck_contains(deck_t * d, card_t c) {
+  for (int ipos=0; ipos < d->n_cards; ipos++) {
+    suit_t suit_ut = d->cards[ipos]->suit;
+    int val_ut = d->cards[ipos]->value;
+    if (suit_ut == c.suit && val_ut == c.value) {
+      return 1;
+    }  // if card found
+  }  // for: all cards in deck
   return 0;
 }
 
 void shuffle(deck_t * d){
+  // shuffle via function `rand`
+  for (int ipos=0; ipos < d->n_cards; ipos++) {
+    int inewpos = rand()%d->n_cards;
 
+    // switch cards with new position
+    card_t *tmp = d->cards[ipos];
+    d->cards[ipos] = d->cards[inewpos];
+    d->cards[inewpos] = tmp;
+  }  // loop all cards in a deck
 }
 
 void assert_full_deck(deck_t * d) {
-
+  for (int ipos=0; ipos < d->n_cards; ipos++) {
+    assert_card_valid(*d->cards[ipos]);
+  }
 }
