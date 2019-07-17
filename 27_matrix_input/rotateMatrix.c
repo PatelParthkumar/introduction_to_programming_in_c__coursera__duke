1a9f14981a045beaa6d099e78de866c168f2d5e6
Coursera Learner <nobody@nowhere.nul>
2019-07-17 14:28:00 +0000

    attempt 2: apparently the tool does not use my own makefile, and cannot handle a separate linked rotate.o file

--- 27_matrix_input/rotateMatrix.c
+++ 27_matrix_input/rotateMatrix.c
@@ -2,7 +2,21 @@
 #include <stdlib.h>
 
 // declaration of rotate
-void rotate(char matrix[10][10]);
+void rotate(char matrix[10][10]) {
+  char matrot[10][10] = {0};
+
+  for (int i0 = 0; i0 < 10; i0++) {
+    for (int i1 = 0; i1 < 10; i1++) {
+      matrot[i1][9-i0] = matrix[i0][i1];
+    }
+  }
+
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 10; j++) {
+      matrix[i][j] = matrot[i][j];
+    }
+  }
+}
 
 void print_matrix(char matrix[10][10]) {
 
