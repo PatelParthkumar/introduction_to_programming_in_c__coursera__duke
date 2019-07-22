76591822ca5267d380cea0f2490b3ba0b86bbc8b
Coursera Learner <nobody@nowhere.nul>
2019-07-22 07:48:25 +0000

    attempt 1: hmm, might be too easy. One question on how the null terminator is added? does strcpy do this?

--- 29_outname/outname.c
+++ 29_outname/outname.c
@@ -4,5 +4,11 @@
 #include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
-  //WRITE ME
+  // malloc outputfilename buffer (add 1 for \0 (NULL terminator))
+  char * outputName = malloc((strlen(inputName) + 7 + 1)*sizeof(inputName[0]));
+
+  // copy filename to new buffer (note: 5 spaces left
+  strcpy(outputName, inputName);
+  strcat(outputName, ".counts");
+  return outputName;
 }
