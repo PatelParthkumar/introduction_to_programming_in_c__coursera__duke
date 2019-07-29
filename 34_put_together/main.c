c05f2dc4f5f43d42bfb1861b32d221212d9fbe98
Coursera Learner <nobody@nowhere.nul>
2019-07-29 12:57:02 +0000

    attempt 1: note that I had to change printCounts to set <unknown> to be the final entry/line in the output file

--- 34_put_together/main.c
+++ 34_put_together/main.c
@@ -7,32 +7,58 @@
 
 counts_t * countFile(const char * filename, kvarray_t * kvPairs) {
   //WRITE ME
-  return NULL;
+  counts_t * c = createCounts();
+
+  // read line by line
+  char * line = NULL;
+  size_t sz = 0;
+  FILE * f = fopen(filename, "r");
+  while (getline(&line, &sz, f) > 0) {
+    // cut of \n
+    line[strlen(line)-1] = '\0';
+    char * value = lookupValue(kvPairs, line);
+    //    printf("key %s -> %s\n", line, value);
+    addCount(c, value);
+  }  // loop all lines
+  fclose(f);
+  free(line);
+  
+  return c;
 }
 
 int main(int argc, char ** argv) {
   //WRITE ME (plus add appropriate error checking!)
- //read the key/value pairs from the file named by argv[1] (call the result kv)
-
+  if (argc < 3) {
+    fprintf(stderr, "call requires a 'kv' filename and one or more 'values' filenames\n");
+    return EXIT_FAILURE;
+  }
+  //read the key/value pairs from the file named by argv[1] (call the result kv)
+  //counts_t * c = createCounts();
+  kvarray_t * kv = readKVs(argv[1]);
+  
  //count from 2 to argc (call the number you count i)
-
+  for (int i = 2; i < argc; ++i) {
     //count the values that appear in the file named by argv[i], using kv as the key/value pair
+    counts_t * c = countFile(argv[i], kv);
     //   (call this result c)
 
     //compute the output file name from argv[i] (call this outName)
-
+    char * outName = computeOutputFileName(argv[i]);
 
     //open the file named by outName (call that f)
-
+    FILE * f = fopen(outName, "w");
     //print the counts from c into the FILE f
-
+    printCounts(c, f);
     //close f
-
+    fclose(f);
     //free the memory for outName and c
-
-
+    freeCounts(c);
+    free(outName);
+    c = NULL;
+  }
 
  //free the memory for kv
+  freeKVs(kv);
 
   return EXIT_SUCCESS;
 }
