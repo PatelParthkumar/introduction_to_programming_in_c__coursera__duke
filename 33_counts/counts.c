1d00d117e3830fffdd9a650c9d61b622d64a4ef6
Coursera Learner <nobody@nowhere.nul>
2019-07-29 12:57:30 +0000

    attempt 1: note that I had to change printCounts to set <unknown> to be the final entry/line in the output file

--- 33_counts/counts.c
+++ 33_counts/counts.c
@@ -16,17 +16,6 @@ counts_t * createCounts(void) {
   return answer;
 }
 
-int check_name_exists(counts_t * c, const char * name) {
-  // check if the name exists in an array
-  for (int i = 0; i < c->nof_elms; ++i) {
-    if (strcmp(c->carr[i]->name, name)) {
-      return i;
-    }
-  }
-
-  return -1;
-}
-
 void addCount(counts_t * c, const char * name) {
   //WRITE ME
   // corner case: name = NULL
@@ -71,9 +60,20 @@ void addCount(counts_t * c, const char * name) {
 
 void printCounts(counts_t * c, FILE * outFile) {
   //WRITE ME
+  int is_unk_present = -1;
   for (int i = 0; i < c->nof_elms; ++i) {
-    fprintf(outFile, "%s: %d\n", c->carr[i]->name, c->carr[i]->count);
+    if (strcmp(c->carr[i]->name, "<unknown>") != 0) {
+      fprintf(outFile, "%s: %d\n", c->carr[i]->name, c->carr[i]->count);
+    }
+    else {
+      is_unk_present = i;
+    }
   }  // loop all keys
+
+  // add unknowns if present
+  if (is_unk_present >= 0) {
+    fprintf(outFile, "<unknown> : %d\n", c->carr[is_unk_present]->count);
+  }
   //  fclose(outFile);
 }
 
