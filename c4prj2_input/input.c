8eed0fb8ed3e5de74c6594e6ac719a23df990a3a
Coursera Learner <nobody@nowhere.nul>
2019-07-30 14:56:47 +0000

    attempt 5: removed all printfs which i left in... sigh

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -20,13 +20,11 @@ deck_t * hand_from_string(const char * str, future_cards_t * fc) {
 
     // check if chard is future card
     if (c_str[0] == '?') {
-      printf("%s is a future card\n", c_str);
       add_empty_card(d);
       size_t index = (size_t)(c_str[1] - '0');
       add_future_card(fc, index, d->cards[d->n_cards-1]);
     }
     else {
-      printf("The card is '%s'\n", c_str);
       card_t c = card_from_letters(c_str[0], c_str[1]);
       add_card_to(d, c);
     }
@@ -37,15 +35,13 @@ deck_t * hand_from_string(const char * str, future_cards_t * fc) {
 
 deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc) {
 
-  printf("Entering *read_input* function\n");
-
   deck_t ** decks = NULL;
   char * line = NULL;
   size_t sz = 0;
   int len = 0;
   while ((len = getline(&line, &sz, f)) >= 0) {
     line[len-1] = '\0';
-    printf("hand %d: '%s'\n", (int)*n_hands, line);
+    //    printf("hand %d: '%s'\n", (int)*n_hands, line);
 
     // check 5 card requirement
     if (len < 14) {
